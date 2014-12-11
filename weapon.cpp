a83cfa210649587175f28f7b04f63ea028e52bb4
Stuart Bowman <stuartsoft2012@gmail.com>
2014-12-11 10:51:45 -0500

    * added weapon class

---
+++ weapon.cpp
@@ -0,0 +1,35 @@
+#include "weapon.h"
+
+
+Weapon::Weapon(){
+	spriteData.width = weaponNS::WIDTH;
+	spriteData.height = weaponNS::HEIGHT;
+	spriteData.x = weaponNS::X;
+	spriteData.y = weaponNS::Y;
+
+	spriteData.rect.bottom = weaponNS::HEIGHT;    // rectangle to select parts of an image
+	spriteData.rect.right = weaponNS::WIDTH;
+	velocity.x = 0;                             // velocity X
+	velocity.y = 0;                             // velocity Y
+	radius = weaponNS::WIDTH/2.0;
+	setScale(weaponNS::SCALE);
+
+	edge.top = -weaponNS::HEIGHT/2;
+	edge.bottom = weaponNS::HEIGHT/2;
+	edge.left = -weaponNS::WIDTH/2;
+	edge.right = -weaponNS::WIDTH/2;
+	collisionType = entityNS::BOX;
+	facingDir = entityNS::direction::none;
+}
+
+void Weapon::draw(VECTOR2 cam){
+	spriteData.x += cam.x;
+	spriteData.y += cam.y;
+	Image::draw();              // draw Player
+	spriteData.x -= cam.x;
+	spriteData.y -= cam.y;
+}
+
+void Weapon::update(float frameTime){
+
+}
---
+++ weapon.h
@@ -0,0 +1,27 @@
+#ifndef _WEAPON_H
+#define _WEAPON_H
+#define WIN32_LEAN_AND_MEAN
+#include "entity.h"
+
+namespace weaponNS{
+	const int WIDTH = 64;                   // image width
+	const int HEIGHT = 64;                  // image height
+	const int X = GAME_WIDTH/2 - WIDTH/2;   // location on screen
+	const int Y = GAME_HEIGHT/2 - HEIGHT/2;
+	const int MAX_SPEED_X = 750;
+	const int MAX_SPEED_Y = 750;
+
+	const int TEXTURE_COL = 4;
+	const float SCALE = 1.0f;
+}
+
+class Weapon: public Entity{
+private:
+	entityNS::direction facingDir;
+public:
+	Weapon();
+	void Weapon::draw(VECTOR2 cam);
+	void Weapon::update(float frameTime);
+};
+
+#endif
